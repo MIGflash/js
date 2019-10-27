3de1f2f3084edf77745c096f924a482c856f7f3c
Irina <9767423@mail.ru>
2019-10-27 11:59:08 +0300

    new

---
+++ javascript/jav1.js
@@ -0,0 +1,25 @@
+let num = 3;
+if (num % 2 === 0){
+	console.log("четное");
+} else {
+	console.log ("не четное");
+}
+
+let n = 8.5;
+let m = 11.45;
+if (n < m){
+   console.log("Ближайщее число к 10 = " + m);
+   }else {
+  console.log("Ближайшее число к 10 = " + n);
+  }
+
+let height = 30; 
+let width  = 20; 
+let length = 50; 
+area = (height * length + height * width + length * width ) * 2;
+	console.log(area);
+	if (height < width) {
+		console.log ("Ширина больше");
+	} else {
+		console.log ("Высота больше");
+	}
